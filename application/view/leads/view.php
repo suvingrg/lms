990ca74ace7f7019b1b4f6af40975f38a202e08f
suvingrg <suvin.gurung@yahoo.com>
2016-01-20 15:39:08 +0545

    update for leads management module

--- application/view/leads/view.php
+++ application/view/leads/view.php
@@ -1,4 +1,3 @@
-
   <div class="mini-container">
     <h2>VIEW / UPDATE LEAD</h2>
     <table>
@@ -18,7 +17,7 @@
                 <td><?php if (isset($lead->l_name)) echo htmlspecialchars($lead->l_name, ENT_QUOTES, 'UTF-8'); ?></td>
                 <td><?php if (isset($lead->address)) echo htmlspecialchars($lead->address, ENT_QUOTES, 'UTF-8'); ?></td>
                 <td><?php if (isset($lead->contact)) echo htmlspecialchars($lead->contact, ENT_QUOTES, 'UTF-8'); ?></td>
-                <td><?//php if (isset($lead->c_id) echo htmlspecialchars($lead->c_id, ENT_QUOTES, 'UTF-8'); ?></td>
+                <td><?php if (isset($lead->c_name)) echo htmlspecialchars($lead->c_name, ENT_QUOTES, 'UTF-8'); ?></td>
                 <td><?php if (isset($lead->status)) echo htmlspecialchars($lead->status, ENT_QUOTES, 'UTF-8'); ?></td>
                 <td><?php if (isset($lead->next_followup)) echo htmlspecialchars($lead->next_followup, ENT_QUOTES, 'UTF-8'); ?></td>
                 <td><a href="<?php echo URL . 'leads/update/' . htmlspecialchars($lead->l_id, ENT_QUOTES, 'UTF-8'); ?>">Update</a></td>
